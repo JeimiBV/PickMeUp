d6b22e031c3872538c9202fbd6574fe614c39eb0
JeimiBV <115201224+JeimiBV@users.noreply.github.com>
2022-10-12 21:29:28 -0400

    El modal ya da

--- Front end/src/componentes/Formulario.jsx
+++ Front end/src/componentes/Formulario.jsx
@@ -1,54 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Modals from "./Modals";
-//import { FormularioEstilos } from "../elementos/FormularioEstilos";
 import "../estilos/formulario.css"
 import { useState } from 'react'
 
 
 
 function Form() {
-                                                                              
-    const [nombre,setNombre]= useState({valor:'', estado:false})
-    const [descripcion,setDescripcion]= useState({valor:'', estado:false})
-    const [producto,setProducto]= useState({valor:'', estado:false})
-    const [archivo,setArchivo]= useState({valor:'', estado:false})
-    const [visible, setVisible]=useState(true)
+
+    const [nombre, setNombre] = useState({ valor: '', estado: false })
+    const [descripcion, setDescripcion] = useState({ valor: '', estado: false })
+    const [producto, setProducto] = useState({ valor: '', estado: false })
+    const [archivo, setArchivo] = useState({ valor: '', estado: false })
+    const [visible, setVisible] = useState(false)
 
 
-    const validacion=()=>{
-        handleShow()
-        
-        if(nombre.valor.length<2||nombre.valor.length>30){
-            setNombre(prevState=>({...prevState, estado:true}))
+
+    const validacion = () => {
+
+        if ((nombre.valor.length >= 2 && nombre.valor.length <= 30) && (descripcion.valor.length >= 10 && descripcion.valor.length <= 100)) {
+            setNombre((prevState) => ({ ...prevState, estado: false }))
+            setDescripcion((prevState) => ({ ...prevState, estado: false }))
+            setVisible(true);
         }
-        else{setNombre(prevState=>({...prevState, estado:false}))} 
-        
-        if(descripcion.valor.length<10||descripcion.valor.length>100){
-            setDescripcion(prevState=>({...prevState, estado:true}))
+        else {
+            setNombre(prevState => ({ ...prevState, estado: true }))
+            setDescripcion(prevState => ({ ...prevState, estado: true }))
         }
-        else{setDescripcion(prevState=>({...prevState, estado:false}))} 
 
     }
 
-    const handleShow=()=>setVisible(!visible)
 
     return <div className="contenedor">
 
-         
+
         <div className="elementos-form">
             <label className="label">
                 Nombre del producto
             </label>
-            <input className="entrada" placeholder="Ingrese el nombre"onChange={ev=> ( setNombre(prevState=>({...prevState, valor:ev.target.value})) )}></input>
-            <h3 className={nombre.estado?"validacion":"invisible"} >
-             El nombre debe contener de 2 a 30 caracteres
+            <input className="entrada" placeholder="Ingrese el nombre" onChange={ev => (setNombre(prevState => ({ ...prevState, valor: ev.target.value })))}></input>
+            <h3 className={nombre.estado ? "validacion" : "invisible"} >
+                El nombre debe contener de 2 a 30 caracteres
             </h3>
             <label className="label">
                 Descripción del producto
             </label>
-            <input className="entrada" placeholder="Ingrese la descripción" onChange={ev=> ( setDescripcion(prevState=>({...prevState, valor:ev.target.value})) )}></input>
-            <h3 className={descripcion.estado?"validacion":"invisible"}>
-             La descripción debe contener de 10 a 100 caracteres
+            <input className="entrada" placeholder="Ingrese la descripción" onChange={ev => (setDescripcion(prevState => ({ ...prevState, valor: ev.target.value })))}></input>
+            <h3 className={descripcion.estado ? "validacion" : "invisible"}>
+                La descripción debe contener de 10 a 100 caracteres
             </h3>
 
             <label className="label">
@@ -69,40 +67,37 @@ function Form() {
                 <option value="postres">Postres</option>
                 <option value="snacks">Snacks</option>
             </select>
-            <h3 className={producto.estado?"validacion":"invisible"}>
-             Se debe seleccionar una opción
+            <h3 className={producto.estado ? "validacion" : "invisible"}>
+                Se debe seleccionar una opción
             </h3>
             <label className="label">
                 Insertar imagen
             </label>
-                <input type="file" className="botonA" />
+            <input type="file" className="botonA" />
 
-
-
-            <button className="botonR"   onClick={validacion}>
+            <button className="botonR" onClick={validacion}>
                 Registrar
             </button>
 
+
+
         </div>
-
-        <span className={visible?"modals" : "transparent"}id="myModal">
-         <h2>
-            Registro de producto
-         </h2>
-         <h3 className="texto-confirmacion">esta seguro de registrar el producto?</h3>
-         <div className="botones">
-            <button className="left">Si</button>
-            <button className="right">No</button>
-         </div>
-        </span>
-
-
-
-
+        <Modals
+            estado={visible}
+            cambiarEstado={setVisible}
+        >
+            <div className="modals">
+                <h2>
+                    Registro de producto
+                </h2>
+                <h3 className="texto-confirmacion">¿Está seguro de registrar el producto?</h3>
+                <div className="botones">
+                    <button className="left">Si</button>
+                    <button className="right">No</button>
+                </div>
+            </div>
+        </Modals>
     </div>
-
-
-
+    
 }
-
 export default Form;
--- Front end/src/componentes/Modals.jsx
+++ Front end/src/componentes/Modals.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import"../estilos/Modals.css"
-function Modals({texto}){
-return(
-    <div className="contenedor-modals">
-        <h2>
-            {texto}
-        </h2>
-    </div>
-)
+import "../estilos/Modals.css"
+
+
+function Modals({ children, estado, cambiarEstado }) {
+    console.log(estado);
+    return (
+        <>
+            {estado &&
+                <div className="over">
+                    {children}
+                </div>
+            }
+        </>
+    )
 
 }
 export default Modals
